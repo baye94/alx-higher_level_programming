5e9d87627632b1671dda966aa221cfd065b9894f
baye94 <fallcheikh893@gmail.com>
2022-06-02 13:44:13 +0000

    0x02-python-import_modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 from sys import argv
-
-
 if __name__ == "__main__":
-    newsum = 0
-    if len(argv) > 1:
-        for i in range(1, len(argv)):
-            newsum += int(argv[i])
-    print(newsum)
-    
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))

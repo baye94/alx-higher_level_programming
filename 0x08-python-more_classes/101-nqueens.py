02fa742b29204588080463dd467f73cfc977f5b0
baye94 <fallcheikh893@gmail.com>
2022-06-27 14:36:06 +0000

    python-more-class

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,62 +1,25 @@
+
+
 #!/usr/bin/python3
-""" ··· N queens ··· """
+# 101-nqueens.py
+"""Solves the N-queens puzzle.
+Determines all possible solutions to placing N
+N non-attacking queens on an NxN chessboard.
+Example:
+    $ ./101-nqueens.py N
+N must be an integer greater than or equal to 4.
+Attributes:
+    board (list): A list of lists representing the chessboard.
+    solutions (list): A list of lists containing solutions.
+Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
+where `r` and `c` represent the row and column, respectively, where a
+queen must be placed on the chessboard.
+"""
 import sys
 
-av = sys.argv
-ac = len(av)
 
-if (ac != 2):
-    print("Usage: nqueens N")
-    sys.exit(1)
-
-try:
-    n = int(av[1])
-except Exception:
-    print("N must be a number")
-    sys.exit(1)
-
-if n < 4:
-    print("N must be at least 4")
-    sys.exit(1)
-
-""" solutions: n - 2 (all positions but borders) """
-nums = []
-for i in range(n + 1):
-    nums.append(i)
-
-""" second position always start in 3 y va sumando de a 2
-    en horizontal la segunda posicion"""
-start = 3
-""" second position increments this way: {2, 3, 4, ..., N} in vertical
-    in bucle hasta N(ex. N = 6,
-    si incremento de a 2 y soy 5 entonces quedo 0)"""
-i = 2
-""" solution number """
-solN = 0
-sol = []
-while(solN < n - 2):
-    sol.append([])
-    for j in range(n):
-        sol[solN].append([j, '-'])
-    sol[solN][0][1] = solN + 1
-    solN += 1
-""" sol[solN][pos][[i],[j]] """
-pos = 1
-while(pos < n):
-    sc = start
-    solN = 0
-    for solN in range(0, n - 2):
-        sol[solN][pos][1] = sc
-        for x in range(i):
-            sc += 1
-            if sc > n:
-                sc = 0
-    for t in range(2):
-        start += 1
-        if (start > n):
-            start = 0
-    i += 1
-    pos += 1
-
-for i in sol:
-    print(i)
+def init_board(n):
+    """Initialize an `n`x`n` sized chessboard with 0's.
+    """
+    board = [['' for col in range(n)] for row in range(n)]
+    return board

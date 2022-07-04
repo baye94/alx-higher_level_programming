ac22591006fb3f85bda8f0bda70cdb4ebfb07647
baye94 <fallcheikh893@gmail.com>
2022-07-04 10:41:57 +0000

    0x0A-python-inheritance

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,39 +1,24 @@
 #!/usr/bin/python3
-"""Integer validator
-Author:Baye cheikh fall
-
+"""
+    Module containing the `Rectangle` class that inherits the `BaseGeometry`
+    class.
 """
 
 
-class BaseGeometry:
-    """A Base Geometry class"""
-
-    def area(self):
-        """Raises an exception because...
-        area is not implemented
-
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates the inputs: name and values
-
-        Args:
-            name(str) - input name as string
-            value(int): pararams validator
-
-        """
-        if type(value) not in [int]:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """A class Rectangle that inherits from BaseGeometry"""
+    """Inherits from the `BaseGeometry` class.
+    """
 
     def __init__(self, width, height):
-        """Instantiation of width and height"""
-        self.integer_validator(width, height)
+        """Initializes the Rectangle instance.
+        Args:
+            width (int): `width` of the `Rectangle` instance.
+            heigth (int): `height` of the `Rectangle` instance.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height

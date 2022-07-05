f8bf010d2ebd443e68481cba6ea986411285e378
baye94 <fallcheikh893@gmail.com>
2022-07-05 21:46:45 +0000

    0x0B-python-input-output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""script that adds all arguments to a Python list
 """
-import sys
+This is the "7-add_item" module.
+Script that adds all arguments to a
+Python list, and then save them to a file
+"""
+from sys import argv
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('8-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+filename = "add_item.json"
+
+try:
+    argv_list = load_from_json_file(filename)
+except FileNotFoundError:
+    argv_list = []
+
+save_to_json_file(argv_list + argv[1:], filename)

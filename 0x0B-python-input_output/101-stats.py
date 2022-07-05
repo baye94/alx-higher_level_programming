987aec0fe563bcebf7fe0f0c257e822e1befa46f
baye94 <fallcheikh893@gmail.com>
2022-07-05 23:12:14 +0000

    0x0B-python-input-output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,38 +1,31 @@
 #!/usr/bin/python3
-import sys
-
-
-def print_status():
-    '''
-        Printing the status of the request
-    '''
-    counter = 0
-    size = 0
-    file_size = 0
-    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                    "403": 0, "404": 0, "405": 0, "500": 0}
-
-    for l in sys.stdin:
-        line = l.split()
-        try:
-            size += int(line[-1])
-            code = line[-2]
-            status_codes[code] += 1
-        except:
-            continue
-        if counter == 9:
-            print("File size: {}".format(size))
-            for key, val in sorted(status_codes.items()):
-                if (val != 0):
-                    print("{}: {}".format(key, val))
-            counter = 0
-        counter += 1
-    if counter < 9:
-        print("File size: {}".format(size))
-        for key, val in sorted(status_codes.items()):
-            if (val != 0):
-                print("{}: {}".format(key, val))
-
 
 if __name__ == "__main__":
-    print_status()
+
+    import sys
+
+    size = 0
+    statuscd = {}
+
+    while True:
+        try:
+            counter = 0
+            for line in sys.stdin:
+                line = line.split()
+
+                size += int(line[8])
+                if statuscd.get(line[7], -1) == -1:
+                    statuscd[line[7]] = 1
+                else:
+                    statuscd[line[7]] += 1
+
+                if counter == 10:
+                    break
+                counter += 1
+
+        except KeyboardInterrupt:
+            exit()
+        finally:
+            print("File size: {}".format(size))
+            for key in sorted(statuscd):
+                print("{}: {}".format(key, statuscd[key]))

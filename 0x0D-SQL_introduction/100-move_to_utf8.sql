b67c0fce54300c8e4625be2d571f8e9f415a1ab4
baye94 <fallcheikh893@gmail.com>
2022-08-09 22:16:19 +0000

    sql-introduction

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,7 +2,7 @@
 -- Use hbtn_0c_0 database
 USE hbtn_0c_0;
 -- Change name to utf-8
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET UTF8;
+ALTER TABLE first_table MODIFY name VARCHAR(255) CHARACTER SET utf8mb4;
 -- Change table to utf-8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Change database to utf-8

5ca8d1c3eb8f770c71631968dd0348fdfa7c4090
baye94 <fallcheikh893@gmail.com>
2022-09-22 18:26:22 +0000

    fix bug

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-# finds a peak in a list of unsorted integers.
+""" Find the peak in a list """
 
 
 def find_peak(list_of_integers):
-    """Find a peak in a list of unsorted integers
-    """
-    if list_of_integers is None or len(list_of_integers) == 0:
+    """Find the peak"""
+    list_l = len(list_of_integers)
+    if list_l is 0:
         return None
+    peak = binary_search(list_of_integers, 0, list_l - 1)
+    return list_of_integers[peak]
 
-    size = len(list_of_integers)
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
 
-    mid = int(size / 2)
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+def binary_search(a, lo, hi):
+    """Recursive binary search of the peak"""
+    if lo >= hi:
+        return lo
+    mid = ((hi - lo) // 2) + lo
+    if a[mid] > a[mid + 1]:
+        return binary_search(a, lo, mid)
     else:
-        return find_peak(list_of_integers[mid + 1:])
+        return binary_search(a, mid + 1, hi)

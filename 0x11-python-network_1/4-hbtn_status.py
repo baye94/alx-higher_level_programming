5a5338500ea4550290eb496e96aaad232f28b6c0
baye94 <fallcheikh893@gmail.com>
2022-09-23 22:16:41 +0000

    fixe bug

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+""" fetches http://alx-intranet/hbtn.io/status
+using the requests library.
 """
-    Module for getting the status of intranet.hbtn.io/status.
-"""
+
 import requests
 
 if __name__ == "__main__":
-    resp = requests.get('https://intranet.hbtn.io/status')
-    if resp is not None:
-        print("Body response:")
-        print("\t- type: {}".format(type(resp.text)))
-        print("\t- content: {}".format(resp.text))
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))

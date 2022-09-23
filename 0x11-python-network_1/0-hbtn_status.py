2fdd6a86968341475e178c9dda444e3f585a8acd
baye94 <fallcheikh893@gmail.com>
2022-09-23 22:21:51 +0000

    fixe bugs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""
-    Module for getting the status of intranet.hbtn.io/status.
-"""
-import urllib.request
+""" fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request as req
+
 
 if __name__ == "__main__":
-    try:
-        with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
-            if resp is not None:
-                html = resp.read()
-                print("Body response:")
-                print("\t- type: {}".format(type(html)))
-                print("\t- content: {}".format(html))
-                print("\t- utf8 content: {}".format(html.decode('utf-8')))
-    except Exception:
-        pass
+    with req.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        data = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode("utf-8")))

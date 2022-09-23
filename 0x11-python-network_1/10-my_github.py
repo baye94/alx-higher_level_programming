5840aba348eedfadfb5f22b305dbb5b3f971492e
baye94 <fallcheikh893@gmail.com>
2022-09-23 22:37:00 +0000

    fix bug

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,11 +1,15 @@
-#!/usr/bin/python3
-"""Takes my GitHub credentials and uses the GitHub API to display my id """
-
-from sys import argv
+!/usr/bin/python3
+"""
+Takes in a string and sends a search request to the Star Wars API.
+"""
 import requests
-from requests.auth import HTTPBasicAuth
+import sys
 
-if __name__ == "__main__":
-    auth = HTTPBasicAuth(argv[1], argv[2])
-    r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+if __name__ == '__main__':
+    user = sys.argv[1]
+    password = sys.argv[2]
+    url = 'https://api.github.com/user'
+
+    r = requests.get(url, auth=(user, password))
+    json_repr = r.json()
+    print(json_repr.get('id', 'None'))

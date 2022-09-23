6cb65c2b143154b8f6ab2688a69de874dfd1f2b0
baye94 <fallcheikh893@gmail.com>
2022-09-23 22:41:43 +0000

    fix bug

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,15 +1,13 @@
-!/usr/bin/python3
-"""
-Takes in a string and sends a search request to the Star Wars API.
-"""
+#!/usr/bin/python3
+"""script to get the id of an authenticated github user"""
+
 import requests
 import sys
 
-if __name__ == '__main__':
-    user = sys.argv[1]
-    password = sys.argv[2]
-    url = 'https://api.github.com/user'
 
-    r = requests.get(url, auth=(user, password))
-    json_repr = r.json()
-    print(json_repr.get('id', 'None'))
+if __name__ == "__main__":
+    search = requests.get(
+        "https://api.github.com/user",
+        auth=(sys.argv[1], sys.argv[2])
+    )
+    print(search.json().get("id"))

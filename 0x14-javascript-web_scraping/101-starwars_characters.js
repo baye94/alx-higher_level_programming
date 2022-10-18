6464f45e5a6cf371fd3836171e48bf81fcacbeaa
baye94 <fallcheikh893@gmail.com>
2022-10-18 22:31:21 +0000

    web scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
+/* script that writes a string to a file
+The first argument is the file path
+The second argument is the string to write
+The content of the file must be read in utf-8
+If an error occurred during the reading, print the error object */
+
 const request = require('request');
+
 require('request').get('https://swapi-api.hbtn.io/api/films/' + process.argv[3] + '/', function (err, r, body) {
   if (err) {
     console.log(err);

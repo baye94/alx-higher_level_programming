c714f1f2c64ff76cb6dbdc39f06a852bde6d38a0
baye94 <fallcheikh893@gmail.com>
2022-10-18 21:35:54 +0000

    web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
-// A script that reads and prints the content of a file
 
-const file = process.argv[2];
-import { readFile } from 'fs';
-
-readFile(file, 'utf8', (error, data) => {
-  if (error) {
-    console.log(error);
+/*
+Web Scraping
+this does some stuff and
+then prints it out
+*/
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
   } else {
     console.log(data);
   }

26a6c6f092dd09c99110391e03a2d6b839b404d3
baye94 <fallcheikh893@gmail.com>
2022-10-18 22:19:46 +0000

    web scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
-
-const myArray = process.argv.slice(2);
 const request = require('request');
-
-if (Number.isInteger(parseInt(myArray[0]))) {
-  request('http://swapi.co/api/films/' + myArray[0], function (error, response, body) {
-    if (error) console.error('error:', error);
-    JSON.parse(body).characters.forEach(link => request(link, (error, response, body) => {
-      if (error) console.error('error:', error);
-      console.log(JSON.parse(body).name);
-    }));
-  });
-}
+const url = 'https://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (!error) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  }
+});

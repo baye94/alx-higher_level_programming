1c0436dee0e99d632f9995c7c1893eac0c88adad
baye94 <fallcheikh893@gmail.com>
2022-10-18 21:40:11 +0000

    web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-
 const fs = require('fs');
+const argms = process.argv[2];
 
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+fs.readFile(argms, 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(data);
+    console.log(data.toString());
   }
 });

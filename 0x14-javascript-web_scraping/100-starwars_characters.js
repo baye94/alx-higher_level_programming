6ca5131c893311978f8bbf49b4e3c904ca840a95
baye94 <fallcheikh893@gmail.com>
2022-10-18 22:05:00 +0000

    web scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,19 +1,15 @@
 #!/usr/bin/node
-/* script that gets the contents of a webpage and stores it in a file
-The first argument is the URL to request
-The second argument the file path to store the body response
-The file must be UTF-8 encoded
-You must use the module request */
+// A script that prints all characters of a Star Wars movie
 
 const request = require('request');
-
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
+const film = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${film}`;
+request(url, (err, res, body) => {
+  if (!err) {
     const characters = JSON.parse(body).characters;
     characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
+      request(character, function (err, res, body) {
+        if (!err) {
           console.log(JSON.parse(body).name);
         }
       });

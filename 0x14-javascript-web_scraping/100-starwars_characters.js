f3f50e92201c5fc951bb9cbc34bf4c5b76005b83
baye94 <fallcheikh893@gmail.com>
2022-10-18 22:11:04 +0000

    web scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,16 +1,13 @@
 #!/usr/bin/node
-// A script that prints all characters of a Star Wars movie
-
 const request = require('request');
-const film = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${film}`;
-request(url, (err, res, body) => {
+const url = 'https://swapi.co/api/films/' + process.argv[2];
+request(url, function (err, response, content) {
   if (!err) {
-    const characters = JSON.parse(body).characters;
+    const characters = JSON.parse(content).characters;
     characters.forEach((character) => {
-      request(character, function (err, res, body) {
+      request(character, function (err, response, content) {
         if (!err) {
-          console.log(JSON.parse(body).name);
+          console.log(JSON.parse(content).name);
         }
       });
     });

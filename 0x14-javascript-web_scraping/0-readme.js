fd194e6b88832b6212579b0c3b893bd41d8b082b
baye94 <fallcheikh893@gmail.com>
2022-10-18 21:23:32 +0000

    web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-/*
-# Write a script that reads and prints the content of a file.
-The first argument is the file path
-*/
+// A script that reads and prints the content of a file
 
-const fs = require('fs');
+const file = process.argv[2];
+import { readFile } from 'fs';
 
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-  if (err) {
-    console.log(err);
+readFile(file, 'utf8', (error, data) => {
+  if (error) {
+    console.log(error);
   } else {
     console.log(data);
   }

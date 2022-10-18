3d26582ea081aec2fb3ca8e6b22989d107c93998
baye94 <fallcheikh893@gmail.com>
2022-10-18 22:34:04 +0000

    web scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,28 @@
 #!/usr/bin/node
-/* script that writes a string to a file
-The first argument is the file path
-The second argument is the string to write
-The content of the file must be read in utf-8
-If an error occurred during the reading, print the error object */
+// A script that prints all characters of a Star Wars movie
+// Display characters name in the same order of the list  “characters” in the /films/ response
 
 const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[3];
 
-require('request').get('https://swapi-api.hbtn.io/api/films/' + process.argv[3] + '/', function (err, r, body) {
+function printCharacters (characters, idx) {
+  request(characters[idx], (err, res, body) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(JSON.parse(body).name);
+      if (idx + 1 < characters.length) {
+        printCharacters(characters, idx + 1);
+      }
+    }
+  });
+}
+
+request(url, (err, res, body) => {
   if (err) {
     console.log(err);
   } else {
-    let i = 0;
-    const actors = JSON.parse(body).characters;
-    for (i = 0; i < actors.length; i++) {
-      request(actors[i], function (error, response, body) {
-        if (error) {
-          console.error('error:', error);
-        }
-        console.log(JSON.parse(body).name);
-      });
-    }
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
